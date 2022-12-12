3851320ff4ac4e5276274957fc8699b648b0ed68
agustina <agustinaderek09@gmail.com>
2022-12-12 08:21:04 -0300

    Subiendo tp

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3030
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/ofertas', (req, res) => res.sendFile(path.join(__dirname, 'views', 'ofertas.html')))
+app.get('/tiendasOficiales', (req, res) => res.sendFile(path.join(__dirname, 'views', 'tiendasOficiales.html')))
+app.get('/vender', (req, res) => res.sendFile(path.join(__dirname, 'views', 'vender.html')))
+app.get('/ayuda', (req, res) => res.sendFile(path.join(__dirname, 'views', 'ayuda.html')))
+
+app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`))
